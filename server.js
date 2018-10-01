8f55152dd5859555fd202431736c75afd2f19276
Demonic30 <koffareza007@gmail.com>
2018-10-01 18:06:25 +0700

    Set db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
+//var db = pgp(process.env.DATABASE_URL);
+var db = pgp('postgres://qczukjeduvghfj:49481b3ac8202f5c86c019f58e76bc22d2697ea6dcc4119eff455d5f00382fbe@ec2-54-243-147-162.compute-1.amazonaws.com:5432/dcdqtmras1p2b8=true');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
