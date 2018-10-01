1fc743abc32bc9d035a00d33f7496cffbf702fd0
Demonic30 <koffareza007@gmail.com>
2018-10-01 16:09:43 +0700

    init repo

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var pgp = require('pg-promise')();
 var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(express.static('static'));
 app.set('view engine', 'ejs');
@@ -35,6 +38,21 @@ app.get('/product', function (req, res) {
             })
     
 });
+
+app.get('/product/:pid', function (req, res) {
+    var pid = req.params.pid;
+    var sql = "select * from products where id=" + pid;
+    db.any(sql)
+        .then(function (data) {
+        res.render('pages/product_edit', { products: data[0] })
+
+    })
+    .catch(function (error) {
+        console.log('ERROR:' + error);
+    })
+    
+});
+
 app.get('/user/:id', function (req, res) {
     var id = req.params.id;
     var sql = 'select * from users';
@@ -63,5 +81,16 @@ app.get('/user', function (req, res) {
             console.log('ERROR:' + error);
         })
 });
+// Update data
+app.post('/product/update', function (req, res) {
+    var id = req.body.id;
+    var title = req.body.title;
+    var price = req.body.price;
+    var sql = `update pproduct set title = $(title), price = $(price) where id = (id)`;
+    // db.none
+    console.log('UPDATE:'+sql);
+    res.redirect('/product');
+});
+ 
 console.log('App is running at http://localhost:8080');
 app.listen(8080);
