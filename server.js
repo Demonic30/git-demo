0e5a8ddd7678a738ec0cd611d502e42e1dd9622e
Demonic30 <koffareza007@gmail.com>
2018-10-20 20:46:05 +0700

    set func insert and app delete

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/about', function (req, res) {
 });
 
 // Display all product
-app.get('/product', function (req, res) {
+app.get('/products', function (req, res) {
     var id = req.param('id');
     var sql = 'select * from products';
     if (id) {
@@ -41,12 +41,13 @@ app.get('/product', function (req, res) {
 
 });
 
-app.get('/product/:pid', function (req, res) {
+app.get('/products/:pid', function (req, res) {
     var pid = req.params.pid;
+    var time = moment().format('MMMM Do YYYY, h:mm:ss a');
     var sql = "select * from products where id=" + pid;
     db.any(sql)
         .then(function (data) {
-            res.render('pages/product_edit', { products: data[0] })
+            res.render('pages/product_edit', { products: data[0],time: time })
 
         })
         .catch(function (error) {
@@ -84,7 +85,7 @@ app.get('/user', function (req, res) {
         })
 });
 // Update data
-app.post('/product/update', function (req, res) {
+app.post('/products/update', function (req, res) {
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
@@ -94,28 +95,47 @@ app.post('/product/update', function (req, res) {
     res.redirect('/product');
 });
 
-app.post('/product/insert', function (req, res){
+app.post('/products/insert', function (req, res){
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
     var sql = `INSERT INTO products (id,title,price) VALUES  ('${id}','${title}','${price}')`;
-    
+    console.log('UPDATE:' + sql);
     db.any(sql)
         .then(function (data) {
+            console.log('DATA:' + data);
             
-            response.redirect('/products')
+            res.redirect('/products')
 
         })
         .catch(function (data) {
-            console.log('ERROR:' + console.error);
+            console.log('ERROR:' + error);
 
         })
 });
 
 app.get('/insert', function (req, res) {
     var time = moment().format('MMMM Do YYYY, h:mm:ss a');
-    res.render('pages/insert)', { time: time});
+    res.render('pages/insert', { time: time});
 });
+
+app.get('/product_delete/:pid',function (req, res) {
+    var id = req.params.pid;
+    var sql = 'DELETE FROM products';
+    if (id){
+            sql += ' where id ='+ id;
+    }
+    db.any(sql)
+        .then(function(data){
+            console.log('DATA:'+data);
+            res.redirect('/products');
+    
+        })
+        .catch(function(data){
+                console.log('ERROR:'+console.error);
+                
+    })
+ });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
